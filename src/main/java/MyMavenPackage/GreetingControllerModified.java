306bc7d78fa7e35f23698ae237073b65cbe53320
wikigain <sandeep.rohilla@gmail.com>
2017-11-05 10:49:18 -0800

    testing builds

--- src/main/java/MyMavenPackage/GreetingControllerModified.java
+++ src/main/java/MyMavenPackage/GreetingControllerModified.java
@@ -1,7 +1,7 @@
 package MyMavenPackage;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+import java.net.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +11,21 @@ public class GreetingControllerModified {
 	  private static final String template = "Hello, %s!";
 	    private final AtomicLong counter = new AtomicLong();
 
-	    @RequestMapping("/sandeep")
+	    @RequestMapping("/neelu")
 	    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	    
+	    try {
+			InetAddress myadd=	InetAddress.getLocalHost();
+			if(myadd!=null)
+			{
+				System.out.println(name+" : "+myadd.getHostName());
+			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.toString());
+		}
+	    
+	    	
 	        return new Greeting(counter.incrementAndGet(),
 	                            String.format(template, name));
 	    }
