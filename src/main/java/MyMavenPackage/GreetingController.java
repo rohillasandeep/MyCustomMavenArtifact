26d0a76c6e7a2ae06741ad4a2aad4971ec277a00
wikigain <sandeep.rohilla@gmail.com>
2019-06-04 08:54:07 -0700

    Update GreetingController.java

--- src/main/java/MyMavenPackage/GreetingController.java
+++ src/main/java/MyMavenPackage/GreetingController.java
@@ -14,6 +14,6 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public Greeting greeting() {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, "World !!!"));
+                            String.format(template, "Container !!!"));
     }
 }
